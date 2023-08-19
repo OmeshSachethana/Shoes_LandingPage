cecd566974c15b19f799c646f195451dd0ae1f15
OmeshSachethana <sachethana12@gmail.com>
2023-08-19 11:31:51 +0530

    updated navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import {headerLogo} from '../assets/images'
+import { hamburger } from '../assets/icons'
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
+            <a href="/">
+                <img src={headerLogo} alt="Logo" width={130} height={29} />
+            </a>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden '>
+                
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
