179cf0987f956a3c184712c87924d2e1295a6fbc
OmeshSachethana <sachethana12@gmail.com>
2023-08-19 11:03:31 +0530

    created files and folder struct

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,18 @@ const App = () => (
     <section className="padding">
       SuperQuality
     </section>
-    <section className="padding">
+    <section className="padding-x py-10 ">
       Services
     </section>
     <section className="padding">
       SpecialOffer
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section><section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section><section className="bg-black padding-x padding-t pb-8">
+      Footer
     </section>
   </main>
 );
