27459a8a3c1401ada8a6b44167018deb2416822b
OmeshSachethana <sachethana12@gmail.com>
2023-08-19 22:26:43 +0530

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import { Hero, Services, SpecialOffer, Subscribe, SuperQuality, Footer, CustomerReviews, PopularProducts } from "./sections";
 import Nav from "./components/Nav";
 const App = () => (
-  <main className="relative">
+  <main className='relative'>
     <Nav />
-    <section className="x1:padding-1 wide:padding-r padding-b ">
+    <section className='xl:padding-l wide:padding-r padding-b'>
       <Hero />
     </section>
-    <section className="padding">
+    <section className='padding'>
       <PopularProducts />
     </section>
-    <section className="padding">
+    <section className='padding'>
       <SuperQuality />
     </section>
-    <section className="padding-x py-10 ">
+    <section className='padding-x py-10'>
       <Services />
     </section>
-    <section className="padding">
+    <section className='padding'>
       <SpecialOffer />
     </section>
-    <section className="bg-pale-blue padding">
+    <section className='bg-pale-blue padding'>
       <CustomerReviews />
-    </section><section className="padding-x sm:py-32 py-16 w-full">
+    </section>
+    <section className='padding-x sm:py-32 py-16 w-full'>
       <Subscribe />
-    </section><section className="bg-black padding-x padding-t pb-8">
+    </section>
+    <section className=' bg-black padding-x padding-t pb-8'>
       <Footer />
     </section>
   </main>
