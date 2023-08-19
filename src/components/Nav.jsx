daa4aae1ccaca8c41c257210931e5731e20033e5
OmeshSachethana <sachethana12@gmail.com>
2023-08-19 11:42:45 +0530

    added nav links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import {headerLogo} from '../assets/images'
 import { hamburger } from '../assets/icons'
+import { navLinks } from '../constants'
 
 const Nav = () => {
   return (
@@ -9,7 +10,13 @@ const Nav = () => {
                 <img src={headerLogo} alt="Logo" width={130} height={29} />
             </a>
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden '>
-                
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray " >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
             </ul>
         </nav>
     </header>
