44dc170d76d270037886db333b5794522f2f249b
OmeshSachethana <sachethana12@gmail.com>
2023-08-19 10:55:36 +0530

    added basic struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="x1:padding-1 wide:padding-r padding-b ">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+  </main>
+);
+
+export default App;
